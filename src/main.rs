a5e807864278d9bc5351bb40986ebf19e32c307c
Jonathan Bosi <jbosi@lucca.fr>
2022-06-25 22:37:08 +0200

    ADD connection to db + model

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,36 @@
-fn main() {
-    println!("Hello, world!");
+mod user_table;
+use user_table::prelude::User;
+
+// Import the needed modules for table creation
+use sea_orm::{ConnectionTrait, Database, Schema};
+// Handle errors using the `https://crates.io/crates/anyhow` crate
+use anyhow::Result;
+
+// Convert this main function into async function
+
+#[async_std::main]
+async fn main() -> Result<()>{
+	// Read the database environment from the `.env` file
+	// let env_database_url: &str = include_str!("../.env").trim();
+	// Split the env url
+	// let split_url: Vec<&str> = env_database_url.split("=").collect();
+	// Get item with the format `database_backend://username:password@localhost/database`
+	// let database_url = split_url[1];
+	let database_url: &str = "postgres://postgres:password@localhost/hcount";
+	
+	let db = Database::connect(database_url)
+		.await
+		.expect("Database connection failed");
+	
+	let builder = db.get_database_backend();
+	let schema = Schema::new(builder);
+	
+	let create_table_op =  db.execute(builder.build(&schema.create_table_from_entity(User))).await;
+	println!("`CREATE TABLE User` {:?}", 
+		match create_table_op {
+			Ok(_) => "Operation Successful".to_owned(),
+			Err(e) => format!("Unsuccessful - Error {:?}", e),
+		}
+	);
+	Ok(())
 }

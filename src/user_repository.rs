115892205489551ca7382bc7c2d3ee8b7281d2c5
Jonathan Bosi <jbosi@lucca.fr>
2022-07-18 21:55:20 +0200

    UPD create user to return created user

--- src/user_repository.rs
+++ src/user_repository.rs
@@ -1,26 +1,27 @@
 use crate::models::{User, NewUser};
 use diesel::prelude::*;
+use diesel::RunQueryDsl;
 use crate::{schema, DbPool};
-use actix_web::{web, get, HttpRequest, Responder, post, Result};
+use actix_web::{web, get, HttpRequest, Responder, post};
 
 #[post("/users")]
-pub async fn create_user(pool: web::Data<DbPool>, new_user: web::Json<NewUser>) -> Result<String> {
+pub async fn create_user(pool: web::Data<DbPool>, new_user: web::Json<NewUser>) -> impl Responder {
 	use schema::users;
 
 	let conn = pool.get().expect("couldn't get db connection from pool");
 
-    let new_user = NewUser {
-        name: new_user.name.to_string(),
-        balance: new_user.balance
-    };
+	let new_user = NewUser {
+		name: new_user.name.to_string(),
+		balance: new_user.balance
+	};
 
-    diesel::insert_into(users::table)
-        .values(&new_user)
-        .execute(&conn)
-        .expect("Error saving new post");
+	let created_user = diesel::insert_into(users::table)
+		.values(&new_user)
+		.get_result::<User>(&conn)
+		.expect("Error saving new post");
 	
 	// TODO send newly created user
-	Ok("User added".to_string())
+	web::Json(created_user)
 }
 
 
@@ -36,7 +37,7 @@ pub async fn get_users(pool: web::Data<DbPool>, _req: HttpRequest) -> impl Respo
 	web::Json(results)
 }
 
-pub fn update_user_amount(pool: web::Data<DbPool>, user_id: i32, amount: f64) -> () {
+pub fn update_user_amount(pool: web::Data<DbPool>, user_id: i32, amount: f64) {
 	use schema::users::dsl::{users, balance};
 
 	let conn = pool.get().expect("couldn't get db connection from pool");
@@ -47,7 +48,7 @@ pub fn update_user_amount(pool: web::Data<DbPool>, user_id: i32, amount: f64) ->
 		.expect("Error while updating user amount");
 }
 
-pub fn delete_user(pool: web::Data<DbPool>, id_to_delete: i32) -> () {
+pub fn delete_user(pool: web::Data<DbPool>, id_to_delete: i32) {
 	use schema::users::dsl::*;
 
 	let conn = pool.get().expect("couldn't get db connection from pool");

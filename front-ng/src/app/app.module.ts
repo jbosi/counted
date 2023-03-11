92df447ca0f18c7a7afa3bf70e34218ff38432d7
jbosi <jbosi@lucca.fr>
2023-03-11 15:00:50 +0100

    ADD cards

--- front-ng/src/app/app.module.ts
+++ front-ng/src/app/app.module.ts
@@ -1,18 +1,28 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { AvatarModule } from 'primeng/avatar';
+import { AvatarGroupModule } from 'primeng/avatargroup';
+import { CardModule } from 'primeng/card';
+import { ProgressBarModule } from 'primeng/progressbar';
+import { TagModule } from 'primeng/tag';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+	declarations: [
+		AppComponent
+	],
+	imports: [
+		BrowserModule,
+		AppRoutingModule,
+		CardModule,
+		AvatarModule,
+		AvatarGroupModule,
+		ProgressBarModule,
+		TagModule
+	],
+	providers: [],
+	bootstrap: [AppComponent]
 })
 export class AppModule { }

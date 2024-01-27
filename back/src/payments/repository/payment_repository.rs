3329afc0e91366e406d392ee2825022dca5987bb
jbosi <jbosi@lucca.fr>
2024-01-27 17:41:22 +0100

    ADD user_id filter on GET payments

--- back/src/payments/repository/payment_repository.rs
+++ back/src/payments/repository/payment_repository.rs
@@ -1,17 +1,31 @@
 use actix_web::web;
-use diesel::RunQueryDsl;
+use actix_web::web::Query;
+use diesel::{QueryDsl, RunQueryDsl};
 
 use crate::{DbPool, schema};
 use crate::payments::domain::payment_model::Payment;
+use crate::payments::domain::payment_query_params::PaymentQueryParams;
 
-pub async fn get_payments(pool: web::Data<DbPool>) -> Vec<Payment> {
+pub async fn get_payments(pool: web::Data<DbPool>, params: Query<PaymentQueryParams>) -> Vec<Payment> {
     use schema::payments::dsl::*;
 
     let mut conn = pool.get().expect("couldn't get db connection from pool");
+    let payments_list: Vec<Payment>;
 
-    let payments_list = payments
-        .load::<Payment>(&mut conn)
-        .expect("Error while trying to get Payment");
+    match params.user_id {
+        None => {
+            payments_list = payments
+                .load::<Payment>(&mut conn)
+                .expect("Error while trying to get Payment");
+        }
+        Some(user_id) => {
+            payments_list = payments
+                .filter(payments::user_id.eq(user_id))
+                .load::<Payment>(&mut conn)
+                .expect("Error while trying to get Payment");
+        }
+    }
+
 
     return payments_list;
 }
--- back/src/payments/web/payment_web.rs
+++ back/src/payments/web/payment_web.rs
@@ -1,9 +1,11 @@
 use actix_web::{get, Responder, web};
 use actix_web::HttpRequest;
+use actix_web::web::Query;
 
 use crate::DbPool;
 use crate::payments::application::payment_application::get_payments_app;
 use crate::payments::domain::payment_model::Payment;
+use crate::payments::domain::payment_query_params::PaymentQueryParams;
 
 #[utoipa::path(
 	responses(
@@ -14,8 +16,9 @@ use crate::payments::domain::payment_model::Payment;
 	)
 )]
 #[get("payments")]
-pub async fn get_payments(pool: web::Data<DbPool>, _req: HttpRequest) -> impl Responder {
-	let payment_list: Vec<Payment> = get_payments_app(pool).await;
+pub async fn get_payments(pool: web::Data<DbPool>, req: HttpRequest) -> impl Responder {
+	let params: Query<PaymentQueryParams> = Query::<PaymentQueryParams>::from_query(req.query_string()).unwrap();
+	let payment_list: Vec<Payment> = get_payments_app(pool, params).await;
 
 	web::Json(payment_list)
 }

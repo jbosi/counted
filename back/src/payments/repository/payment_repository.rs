7b2e7c28b7aaa46360ea6237221df17d84d07693
jbosi <jbosi@lucca.fr>
2024-03-25 23:50:20 +0100

    FIX payment_repository

--- back/src/payments/repository/payment_repository.rs
+++ back/src/payments/repository/payment_repository.rs
@@ -6,13 +6,12 @@ use diesel::prelude::*;
 use crate::{DbPool, schema};
 use crate::payments::domain::payment_model::Payment;
 use crate::payments::domain::payment_query_params::PaymentQueryParams;
-use crate::schema::expenses::dsl::expenses;
 
 pub async fn get_payments(pool: web::Data<DbPool>, params: Query<PaymentQueryParams>) -> Vec<Payment> {
     use schema::payments::dsl::*;
 
     let mut conn = pool.get().expect("couldn't get db connection from pool");
-    let mut query = expenses.into_boxed();
+    let mut query = payments.into_boxed();
 
     if let Some(user_id_unwrapped) = params.user_id {
         query = query.filter(user_id.eq(user_id_unwrapped))

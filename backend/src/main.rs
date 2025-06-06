c086a445a3cee875173489734a4f8fe09501496b
jbosi <jonathan.bosi@lilo.org>
2025-06-06 13:27:11 +0200

    Fix errors and add .env

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,5 +1,7 @@
 use axum::{routing::get, Router, Json};
 use shared::User;
+use std::env;
+use dotenvy::dotenv;
 
 async fn get_user() -> Json<User> {
     Json(User {
@@ -11,10 +13,14 @@ async fn get_user() -> Json<User> {
 
 #[tokio::main]
 async fn main() {
+    dotenv().ok(); // charge le .env
+    let port = env::var("PORT").unwrap_or_else(|_| "3000".to_string());
+    let addr = format!("0.0.0.0:{}", port);
+
     let app = Router::new().route("/user", get(get_user));
 
     println!("Server running on http://localhost:3000");
-    axum::Server::bind(&"0.0.0.0:3000".parse().unwrap())
+    axum::Server::bind(&addr.parse().unwrap())
         .serve(app.into_make_service())
         .await
         .unwrap();

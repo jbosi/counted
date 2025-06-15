c74c0574c418c2d0baaf80832b13cd5635383e47
jbosi <jonathan.bosi@lilo.org>
2025-06-15 13:38:01 +0200

    ADD basic card

--- web/src/views/home.rs
+++ web/src/views/home.rs
@@ -4,7 +4,41 @@ use ui::{Echo, Hero};
 #[component]
 pub fn Home() -> Element {
     rsx! {
-        Hero {}
+        div {
+            class: "cards-container",
+            div {
+                class: "card",
+                h3 {
+                    class: "",
+                    "Voyage en Allemagne"
+                }
+                p {
+                    class: "",
+                    "De la bière !"
+                }
+                button {
+                    class: "button-action",
+                    r#type: "button",
+                    "En cours"
+                }
+            }
+            div {
+                class: "card",
+                h3 {
+                    class: "",
+                    "Voyage en Italie"
+                }
+                p {
+                    class: "",
+                    "Petit voyage à 4 au soleil"
+                }
+                button {
+                    class: "button-action",
+                    r#type: "button",
+                    "En cours"
+                }
+            }
+        }
         Echo {}
     }
 }

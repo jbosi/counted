e9902d1cb8a93379897cc2b52d61c5f55a2ccb61
Jonathan Bosi <61420345+jbosi@users.noreply.github.com>
2025-08-31 14:23:29 +0200

    Improve expense form (#20)
    * FIX: style
    
    * ADD: callout on amount mismatch and callout component

--- ui/src/modals/add_expense_modal.rs
+++ ui/src/modals/add_expense_modal.rs
@@ -1,4 +1,6 @@
+use crate::common::{CalloutComponent, CalloutComponentTypes};
 use api::expenses::add_expense;
+use dioxus::logger::tracing::info;
 use dioxus::prelude::*;
 use shared::{CreatableExpense, ExpenseType, User, UserAmount};
 use uuid::Uuid;
@@ -23,6 +25,7 @@ pub fn AddExpenseModal(mut props: AddExpenseModalProps) -> Element {
     let mut expense_name: Signal<String> = use_signal(|| "".to_string());
     let mut expense_description: Signal<Option<String>> = use_signal(|| None);
     let expense_type: Signal<ExpenseType> = use_signal(|| ExpenseType::Expense);
+    let mut has_sent_form: Signal<bool> = use_signal(|| false);
 
     let checkbox_list: Vec<FormCheckboxAmount> = props
         .users
@@ -38,12 +41,19 @@ pub fn AddExpenseModal(mut props: AddExpenseModalProps) -> Element {
     let mut expense_debtors: Signal<Vec<FormCheckboxAmount>> = use_signal(|| checkbox_list.clone());
     let mut expense_payers: Signal<Vec<FormCheckboxAmount>> = use_signal(|| checkbox_list.clone());
 
+    let payers_total: Memo<f64> = use_memo(move || {
+        expense_payers().iter().map(|e| e.amount).reduce(|acc, e| acc + e).unwrap_or(0.0)
+    });
+    let debtors_total = use_memo(move || {
+        expense_debtors().iter().map(|d| d.amount).reduce(|acc, e| acc + e).unwrap_or(0.0)
+    });
+    let has_expense_amount_mismatch = use_memo(move || payers_total() != debtors_total());
+
     let handle_checkbox_change_debtors = move |data: (usize, bool)| {
         let (index, checked) = data;
         expense_debtors.with_mut(|items| {
             if let Some(item) = items.get_mut(index) {
                 item.is_checked = checked;
-                // Optionnel : vider le texte si la case est décochée
                 if !checked {
                     item.amount = 0.0;
                 }
@@ -56,7 +66,6 @@ pub fn AddExpenseModal(mut props: AddExpenseModalProps) -> Element {
         expense_payers.with_mut(|items| {
             if let Some(item) = items.get_mut(index) {
                 item.is_checked = checked;
-                // Optionnel : vider le texte si la case est décochée
                 if !checked {
                     item.amount = 0.0;
                 }
@@ -108,7 +117,7 @@ pub fn AddExpenseModal(mut props: AddExpenseModalProps) -> Element {
                     }
                 }
                 fieldset { class: "fieldset",
-                    legend { class: "fieldset-legend", "Qui a payé ?" }
+                    legend { class: "fieldset-legend", "Qui a payé ? Total : {payers_total} €" }
                     for (index , item) in expense_payers().iter().enumerate() {
                         CheckboxFormItem {
                             index,
@@ -119,7 +128,7 @@ pub fn AddExpenseModal(mut props: AddExpenseModalProps) -> Element {
                     }
                 }
                 fieldset { class: "fieldset",
-                    legend { class: "fieldset-legend", "Qui a doit rembourser ?" }
+                    legend { class: "fieldset-legend", "Qui doit rembourser ? Total : {debtors_total} €" }
                     for (index , item) in expense_debtors().iter().enumerate() {
                         CheckboxFormItem {
                             index,
@@ -128,6 +137,9 @@ pub fn AddExpenseModal(mut props: AddExpenseModalProps) -> Element {
                             on_amount_change: handle_amount_change_debtors,
                         }
                     }
+                }
+                if has_expense_amount_mismatch() && has_sent_form() {
+                    CalloutComponent { callout_type: CalloutComponentTypes::error, error_message: "payers_total : {payers_total()} € is different from debtors_total : {debtors_total()} €" }
                 }
                 form {
                     method: "dialog",
@@ -141,6 +153,7 @@ pub fn AddExpenseModal(mut props: AddExpenseModalProps) -> Element {
                         onclick: move |_| {
                             let users = props.users.clone();
                             spawn(async move {
+                                has_sent_form.set(true);
                                 let creatable_expense: CreatableExpense = CreatableExpense {
                                     name: expense_name(),
                                     description: expense_description(),
@@ -167,8 +180,13 @@ pub fn AddExpenseModal(mut props: AddExpenseModalProps) -> Element {
                                         .collect(),
                                     author_id: users[0].id,
                                 };
-                                add_expense(creatable_expense).await.expect("Failed to add new expense");
-                                props.is_expense_modal_open.set(false)
+
+                                if has_expense_amount_mismatch() {
+                                    info!("payers_total : {:?} is different from total_debtors : {:?}", payers_total(), debtors_total())
+                                } else {
+                                    add_expense(creatable_expense).await.expect("Failed to add new expense");
+                                    props.is_expense_modal_open.set(false);
+                                }
                             });
                         },
                         "Enregistrer"
@@ -193,26 +211,29 @@ fn CheckboxFormItem(
     on_amount_change: EventHandler<(usize, f64)>,
 ) -> Element {
     rsx! {
-        div { class: "flex items-center space-x-3 p-3 border rounded-lg",
+        div { class: "flex items-center space-x-3 p-3 border rounded-lg justify-between",
 
-            // Checkbox
-            input {
-                r#type: "checkbox",
-                class: "h-4 w-4 text-blue-600 focus:ring-blue-500 border-gray-300 rounded",
-                checked: item.is_checked,
-                onchange: move |evt| {
-                    on_checkbox_change.call((index, evt.checked()));
-                },
+            div {
+                class: "space-x-3",
+                // Checkbox
+                input {
+                    r#type: "checkbox",
+                    class: "h-4 w-4 text-blue-600 focus:ring-blue-500 border-gray-300 rounded",
+                    checked: item.is_checked,
+                    onchange: move |evt| {
+                        on_checkbox_change.call((index, evt.checked()));
+                    },
+                }
+
+                // Label
+                label { class: "text-sm font-medium text-base-content", "{item.label}" }
             }
-
-            // Label
-            label { class: "text-sm font-medium text-base-content", "{item.label}" }
 
             // Input number
             input {
                 r#type: "number",
                 class: format!(
-                    "flex-1 px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-blue-500 focus:border-blue-500 {}",
+                    "px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-blue-500 focus:border-blue-500 {}",
                     if item.is_checked { "" } else { "bg-gray-500 cursor-not-allowed" },
                 ),
                 disabled: !item.is_checked,

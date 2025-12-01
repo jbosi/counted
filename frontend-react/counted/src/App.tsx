bf62cb4f9530ce7b42c5809e2442f5c6f3e51cb0
jbosi <jonathan.bosi@lilo.org>
2025-12-02 00:09:39 +0100

    ADD: router

--- frontend-react/counted/src/App.tsx
+++ frontend-react/counted/src/App.tsx
@@ -1,13 +1,21 @@
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import './App.css';
 import { Projects } from './pages/projects/projects';
+import { createBrowserRouter } from 'react-router';
+import { RouterProvider } from 'react-router/dom';
 
+const router = createBrowserRouter([
+	{
+		path: '/',
+		element: <Projects />,
+	},
+]);
 const queryClient = new QueryClient();
 
 function App() {
 	return (
 		<QueryClientProvider client={queryClient}>
-			<Projects></Projects>
+			<RouterProvider router={router} />
 		</QueryClientProvider>
 	);
 }

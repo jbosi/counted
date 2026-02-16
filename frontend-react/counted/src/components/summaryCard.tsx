fb6d6e935d1d161a8f80ed5d999694004e907bef
jbosi <jonathan.bosi@lilo.org>
2026-02-16 18:19:30 +0100

    FIX summarycard logic and style

--- frontend-react/counted/src/components/summaryCard.tsx
+++ frontend-react/counted/src/components/summaryCard.tsx
@@ -24,29 +24,27 @@ export const SummaryCard = ({ globalTotal, projectId }: SummaryCardProps) => {
 	// const userId: number | undefined = getProjectUserIdFromLocalstorage(countedLocalStorage, projectId);
 	// const me: User | undefined = users.find((u) => u.id === userId);
 
-	const myPaymentsSum = useMemo(() => {
+	const myPaymentsSum: number = useMemo(() => {
 		if (!payments || storedUserId == null) {
-			return null;
+			return 0;
 		}
 		return payments
-			.filter((p) => p.userId === storedUserId)
+			?.filter((p) => p.userId === storedUserId)
 			.map((val) => val.amount)
-			.reduce((acc, val) => acc + val);
+			.reduce((acc, val) => acc + val, 0);
 	}, [payments, storedUserId]);
 
 	return (
 		<div>
-			<div className="card-body p-4 space-y-3">
+			<div className="card-body p-4">
 				<div className="flex justify-between items-center">
-					<span className="text-base-content/70">Total des dépenses</span>
-					<span className="font-semibold text-lg text-base-content/70">{formatCurrency(globalTotal ?? 0)}</span>
+					<span className="text-neutral-500">Total des dépenses</span>
+					<span className="font-semibold text-lg text-neutral-600">{formatCurrency(globalTotal ?? 0)}</span>
 				</div>
-				{myPaymentsSum && (
-					<div className="flex justify-between items-center">
-						<span className="text-base-content/70">Mes dépenses</span>
-						<span className="font-semibold text-lg text-base-content/70">{formatCurrency(myPaymentsSum)}</span>
-					</div>
-				)}
+				<div className="flex justify-between items-center">
+					<span className="text-neutral-500">Mes dépenses</span>
+					<span className="font-semibold text-lg text-neutral-600">{formatCurrency(myPaymentsSum)}</span>
+				</div>
 			</div>
 		</div>
 	);

40034e6c2bb002b5c81ea8f693dd20bc942f06eb
jbosi <jonathan.bosi@lilo.org>
2026-03-17 23:33:22 +0100

    FEAT: add mobile

--- packages/mobile/src/main.rs
+++ packages/mobile/src/main.rs
@@ -1,27 +1,41 @@
+use api::auth::auth_controller::me;
 use dioxus::prelude::*;
+use shared::Account;
+use ui::common::{read_from_ls, LocalStorageState};
 use ui::route::Route;
 
 const FAVICON: Asset = asset!("/assets/favicon.ico");
 const MAIN_CSS: Asset = asset!("/assets/main.css");
-const TAILWIND_CSS: Asset = asset!("/assets/tailwind.css");
 
 fn main() {
+    dioxus::logger::initialize_default();
     dioxus::launch(app);
 }
 
 #[component]
 fn app() -> Element {
+    let auth: Signal<Option<Account>> = use_context_provider(|| Signal::new(None));
+    let _ls: Signal<LocalStorageState> = use_context_provider(|| Signal::new(read_from_ls()));
+
+    use_effect(move || {
+        let mut auth = auth;
+        spawn(async move {
+            if let Ok(account) = me().await {
+                auth.set(account);
+            }
+        });
+    });
+
     rsx! {
-        // Global app resources
         document::Link { rel: "icon", href: FAVICON }
         document::Link { rel: "stylesheet", href: MAIN_CSS }
-        document::Link { rel: "stylesheet", href: TAILWIND_CSS }
 
-        main { class: "min-h-screen flex flex-col items-center",
-            link {
-                rel: "stylesheet",
-                href: "https://unpkg.com/tailwindcss@^2.0/dist/tailwind.min.css",
-            }
+        main {
+            "data-theme": "cupcake",
+            class: "min-h-screen flex flex-col items-center",
+            script { src: "https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4" }
+            link { rel: "stylesheet", href: "https://cdn.jsdelivr.net/npm/daisyui@5" }
+            link { rel: "stylesheet", href: "https://cdn.jsdelivr.net/npm/daisyui@5/themes.css" }
             Router::<Route> {}
         }
     }
